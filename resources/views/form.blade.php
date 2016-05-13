f189996ad3a853d77665f353063c07a9a8044cc2
bajke <salebdj@gmail.com>
2016-05-13 13:35:01 +0200

    back to basics, still looking into ajax

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,9 +1,9 @@
 <form action="" method="POST">
-    {{--<label for="phone_ip">IP address of phone <br>--}}
-        {{--<input type="text" name="phone_ip" id="phone_ip" />--}}
-    {{--</label>--}}
-    {{--<br>--}}
-    {{--<br>--}}
+    <label for="phone_ip">IP address of phone <br>
+        <input type="text" name="phone_ip" id="phone_ip" />
+    </label>
+    <br>
+    <br>
     <label for="number">Phone number to call <br>
         <input type="text" name="number" id="number" />
     </label>
