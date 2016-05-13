b6f122001f870d36e6f6ba10e3e2459b4e358a72
bajke <salebdj@gmail.com>
2016-05-13 12:18:23 +0200

    lets see if it works with ajax post

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,12 +1,37 @@
 <form action="" method="POST">
-    <label for="phone_ip">IP address of phone <br>
-        <input type="text" name="phone_ip" id="phone_ip" />
-    </label>
-    <br>
-    <br>
+    {{--<label for="phone_ip">IP address of phone <br>--}}
+        {{--<input type="text" name="phone_ip" id="phone_ip" />--}}
+    {{--</label>--}}
+    {{--<br>--}}
+    {{--<br>--}}
     <label for="number">Phone number to call <br>
         <input type="text" name="number" id="number" />
     </label>
     <br>
     <input type="submit" />
 </form>
+
+<label for="phone_ip">IP address of phone <br>
+    <input type="text" name="phone_ip" id="phone_ip" />
+</label>
+<br />
+
+<button onclick="call203();">203</button>
+
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
+<script>
+
+    function call203(){
+        $.ajax({
+            type: "POST",
+            url: "http://" + $('#phone_ip').val() + "/push",
+            contentType: "application/x-com-polycom-spipx",
+            data: '<PolycomIPPhone><Data priority="Critical">tel://203</Data></PolycomIPPhone>',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    }
+
+</script>
